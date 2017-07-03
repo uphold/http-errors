5061c6ec16e6f488737814b851133fd564673318
Ricardo Gama <rgama@uphold.com>
2017-07-03 10:47:11 +0100

    Add ServiceUnavailableError

---
+++ src/errors/service-unavailable-error.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+const HttpError = require('./http-error');
+
+/**
+ * Export "Service Unavailable" error.
+ */
+
+module.exports = class ServiceUnavailableError extends HttpError {
+
+  /**
+   * Constructor.
+   */
+
+  constructor() {
+    super(503, ...arguments);
+  }
+};
